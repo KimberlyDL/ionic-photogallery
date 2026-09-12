3e3fcefe16b7106909410d6a71c8c5136f88da30
KimberlyDL <1d3.de.leon.kimberly.nicole.i@gmail.com>
2026-09-13 01:28:56 +0800

    Initial Commit

--- src/components/PhotoGalleryComponent.vue
+++ src/components/PhotoGalleryComponent.vue
@@ -6,12 +6,34 @@
       </ion-col>
     </ion-row>
   </ion-grid>
+  <div v-else class="empty-state">
+    <ion-icon :icon="imagesOutline" />
+    <p>No photos yet. Tap "Take Picture" to add one.</p>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { IonGrid, IonRow, IonCol, IonImg } from "@ionic/vue";
+import { IonGrid, IonRow, IonCol, IonImg, IonIcon } from "@ionic/vue";
+import { imagesOutline } from "ionicons/icons";
 
 defineProps<{
   photos: string[];
 }>();
 </script>
+
+<style scoped>
+.empty-state {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 48px 16px;
+  color: var(--ion-color-medium);
+  text-align: center;
+}
+
+.empty-state ion-icon {
+  font-size: 48px;
+  margin-bottom: 12px;
+}
+</style>
